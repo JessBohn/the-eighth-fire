a70f2132b7b06f9c572c3a0cfa6736937d8ee865
Jessica Bohn <bohn.16@wright.edu>
2016-10-21 15:06:23 -0500

    Add multiple columns for password_digest and others
    The users table did not have a place for their password to be saved
    so the password_digest attribute was added, this must be specifically
    named password_digest to work with the BCrypt gem. A remember digest
    and reset digest were also added so that a password change for a user
    can be implemented on the website if forgotten or if they want to.

--- db/migrate/20161020005151_create_users.rb
+++ db/migrate/20161020005151_create_users.rb
@@ -3,7 +3,11 @@ class CreateUsers < ActiveRecord::Migration[5.0]
     create_table :users do |t|
       t.string :name
       t.string :email
+      t.string :password_digest
       t.boolean :newsletter
+      t.string :remember_digest
+      t.string :reset_digest
+      t.datetime :reset_sent_at
 
       t.timestamps
     end
